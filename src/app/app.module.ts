2e827ddb80469dbde781d199aa9dcc6e6fd9e54f
harold alzate <haroldalzate99@gmail.com>
2022-11-11 16:26:41 -0500

    agredando secciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { ProfesionalesComponent } from './profesionales/profesionales.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadosComponent,
-    EmpleadoComponent
+    EmpleadoComponent,
+    ProfesionalesComponent,
+    ServiciosComponent,
+    ProyectosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ProyectosComponent,ServiciosComponent,ProfesionalesComponent]
 })
 export class AppModule { }
